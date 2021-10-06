be963e0d87674473c7d3bc9194f13048b85b3924
Rekha Rani <75579258+rekha2023mca1096@users.noreply.github.com>
2021-10-06 10:20:49 +0530

    callback_second.js

---
+++ callback_second.js
@@ -0,0 +1,11 @@
+console.log("start");
+function getName(name){
+    //return name;
+    setTimeout(function(){
+        return name;
+
+    },20000)
+}
+const uName=getName('REKHA RANI');
+console.log(uName);
+console.log("END");
